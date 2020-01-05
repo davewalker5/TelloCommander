b0749985400a4ace1e1074af7d633411af86d204
dave <david.walker5@icloud.com>
2020-01-05 23:46:00 +0000

    Unit test  coverage improvements

---
+++ src/TelloCommander.Tests/MockDroneTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TelloCommander.CommandDictionaries;
+using TelloCommander.Connections;
+
+namespace TelloCommander.Tests
+{
+    [TestClass]
+    public class MockDroneTest
+    {
+        private MockDrone _drone;
+
+        [TestInitialize]
+        public void TestInitialise()
+        {
+            CommandDictionary dictionary = CommandDictionary.ReadStandardDictionary("1.3.0.0");
+            _drone = new MockDrone(dictionary);
+        }
+
+        [TestMethod]
+        public void StopSimulatorTest()
+        {
+            Assert.IsFalse(_drone.Stop);
+            string response = _drone.ConstructCommandResponse("stopsimulator");
+            Assert.AreEqual("ok", response);
+            Assert.IsTrue(_drone.Stop);
+        }
+
+        [TestMethod]
+        public void ResponseDelayTest()
+        {
+            Assert.AreEqual(0, _drone.ResponseDelay);
+            string response = _drone.ConstructCommandResponse("responsedelay 10");
+            Assert.AreEqual("ok", response);
+            Assert.AreEqual(10, _drone.ResponseDelay);
+        }
+    }
+}
